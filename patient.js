535be8d99775d788d23de81290a2a150b13c3a91
Rudra-Surati-code <73886769+Rudra-Surati-code@users.noreply.github.com>
2021-07-12 19:16:22 +0530

    Add files via upload

--- patient.js
+++ patient.js
@@ -1,3 +1,31 @@
 function procress() {
   document.querySelector(".loader").classList.add("d-none");
 }
+
+document.querySelector(".home").addEventListener("click", function() {
+  document.querySelector(".home").classList.add("active");
+  document.querySelector(".folder").classList.remove("active");
+  document.querySelector(".comment").classList.remove("active");
+  document.querySelector(".phone").classList.remove("active");
+})
+
+document.querySelector(".folder").addEventListener("click", function() {
+  document.querySelector(".home").classList.remove("active");
+  document.querySelector(".folder").classList.add("active");
+  document.querySelector(".comment").classList.remove("active");
+  document.querySelector(".phone").classList.remove("active");
+})
+
+document.querySelector(".comment").addEventListener("click", function() {
+  document.querySelector(".home").classList.remove("active");
+  document.querySelector(".folder").classList.remove("active");
+  document.querySelector(".comment").classList.add("active");
+  document.querySelector(".phone").classList.remove("active");
+})
+
+document.querySelector(".phone").addEventListener("click", function() {
+  document.querySelector(".home").classList.remove("active");
+  document.querySelector(".folder").classList.remove("active");
+  document.querySelector(".comment").classList.remove("active");
+  document.querySelector(".phone").classList.add("active");
+})
