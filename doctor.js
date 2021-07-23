54b2d0e606fe92692cb36561ce8f70f3704ad00a
Rudra-Surati-code <73886769+Rudra-Surati-code@users.noreply.github.com>
2021-07-23 08:54:48 +0530

    Add files via upload

--- doctor.js
+++ doctor.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
     $(".loader").css('display', "none")
-    $(".username").html(localStorage.getItem("name"))
+    $(".username").html(localStorage.getItem("name"));
+    if(localStorage.getItem("Doctor Specialist") == null) {
+        console.log()
+    } else {
+        $(".doctor-type").css('display', "none");
+    }
+})
+
+$(".doctor-type-input-button").click(function() {
+    const letters = /^[A-Za-z]+$/;
+    if($(".doctor-type-input").val() != letters) {
+        $(".error-doctor-type-input").html("Please Enter Correct Doctor Specialist")
+    } else {
+        localStorage.setItem("Doctor Specialist", $(".doctor-type-input").val())
+        location.reload()
+    }
 })
--- sign up.js
+++ sign up.js
@@ -1,14 +1,13 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCk5s2RXo09ai0yHEvv8PVC7P_FsgA0dJk",
-    authDomain: "paper-less-sign-in-and-sign-up.firebaseapp.com",
-    databaseURL: "https://paper-less-sign-in-and-sign-up-default-rtdb.firebaseio.com",
-    projectId: "paper-less-sign-in-and-sign-up",
-    storageBucket: "paper-less-sign-in-and-sign-up.appspot.com",
-    messagingSenderId: "706828671055",
-    appId: "1:706828671055:web:133a0e1193fa42aeb9f23a",
-    measurementId: "G-7G58N240JQ"
+var firebaseConfig = {
+    apiKey: "AIzaSyDEVFkiLOkEcwwIr-3YgO_FBY5wbYYAIcU",
+    authDomain: "paper-less-5c7a0.firebaseapp.com",
+    databaseURL: "https://paper-less-5c7a0-default-rtdb.firebaseio.com",
+    projectId: "paper-less-5c7a0",
+    storageBucket: "paper-less-5c7a0.appspot.com",
+    messagingSenderId: "20247367059",
+    appId: "1:20247367059:web:1ae117fa5dfdaf1a2c6c66",
+    measurementId: "G-8L7E8S91DR"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
